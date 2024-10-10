41bd53c262780177f9cbfeeb228da6a371052d83
haseeebA <25100122@lums.edu.pk>
2024-10-11 01:02:17 +0500

    commit

--- new.js
+++ new.js
@@ -48,6 +48,6 @@ app.post('/send-message', async (req, res) => {
 
 // Set up the server to listen for API calls
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
